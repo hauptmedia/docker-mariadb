e0e4e1c6ca6be8d5c8a4aa58a29c118f9b9bc02f
Julian Haupt <julian.haupt@hauptmedia.de>
2015-09-22 15:32:07 +0200

    make docker-entrypoint script work with both galera and non-galera environments

--- 10.1/docker-entrypoint.sh
+++ 10.1/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+DATADIR=/var/lib/mysql
 
 # set timezone if it was specified
 if [ -n "$TIMEZONE" ]; then
@@ -14,26 +15,28 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'mysqld' ]; then
-	if [ -z "$CLUSTER_NAME" ]; then
-		echo >&2 'error:  missing CLUSTER_NAME'
-		echo >&2 '  Did you forget to add -e CLUSTER_NAME=... ?'
-		exit 1
-	fi
+	if [ -n "$GALERA" ]; then
+		if [ -z "$CLUSTER_NAME" ]; then
+			echo >&2 'error:  missing CLUSTER_NAME'
+			echo >&2 '  Did you forget to add -e CLUSTER_NAME=... ?'
+			exit 1
+		fi
 	
-	if [ -z "$NODE_NAME" ]; then
-		echo >&2 'error:  missing NODE_NAME'
-		echo >&2 '  Did you forget to add -e NODE_NAME=... ?'
-		exit 1
-	fi
+		if [ -z "$NODE_NAME" ]; then
+			echo >&2 'error:  missing NODE_NAME'
+			echo >&2 '  Did you forget to add -e NODE_NAME=... ?'
+			exit 1
+		fi
 	
-	if [ -z "$CLUSTER_ADDRESS" ]; then
-		echo >&2 'error:  missing CLUSTER_ADDRESS'
-		echo >&2 '  Did you forget to add -e CLUSTER_ADDRESS=... ?'
-		exit 1
+		if [ -z "$CLUSTER_ADDRESS" ]; then
+			echo >&2 'error:  missing CLUSTER_ADDRESS'
+			echo >&2 '  Did you forget to add -e CLUSTER_ADDRESS=... ?'
+			exit 1
+		fi
+	else
+		rm /etc/mysql/conf.d/galera.cnf
 	fi
 
-	# read DATADIR from the MySQL config
-	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
 	
 	if [ ! -d "$DATADIR/mysql" ]; then
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
@@ -42,7 +45,7 @@ if [ "$1" = 'mysqld' ]; then
 			exit 1
 		fi
 
-		if [ -z "$REPLICATION_PASSWORD" ]; then
+		if [ -n "$GALERA" -a -z "$REPLICATION_PASSWORD" ]; then
 			echo >&2 'error:  missing REPLICATION_PASSWORD'
 			echo >&2 '  Did you forget to add -e REPLICATION_PASSWORD=... ?'
 			exit 1
@@ -62,12 +65,16 @@ if [ "$1" = 'mysqld' ]; then
 			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
-			CREATE USER 'replication'@'%' IDENTIFIED BY '${REPLICATION_PASSWORD}';
-			GRANT RELOAD,LOCK TABLES,REPLICATION CLIENT ON *.* TO 'replication'@'%';
-
-			DROP DATABASE IF EXISTS test ;
+			DROP DATABASE IF EXISTS test;
 		EOSQL
 		
+		if [ -n "$GALERA" ]; then
+			cat >> "$tempSqlFile" <<-EOSQL
+			CREATE USER 'replication'@'%' IDENTIFIED BY '${REPLICATION_PASSWORD}';
+			GRANT RELOAD,LOCK TABLES,REPLICATION CLIENT ON *.* TO 'replication'@'%';
+			EOSQL
+		fi
+
 		if [ "$MYSQL_DATABASE" ]; then
 			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
 		fi
@@ -85,18 +92,24 @@ if [ "$1" = 'mysqld' ]; then
 		set -- "$@" --init-file="$tempSqlFile"
 
 	else
-		WSREP_START_POSITION="$("$@" --wsrep-recover --log-error=/dev/stdout 2>&1 | grep 'Recovered position' | awk '{print $NF}')" 
-		echo WSREP: Using start position ${WSREP_START_POSITION}
-		set -- "$@" --wsrep_start_position=${WSREP_START_POSITION}
+		if [ -n "$GALERA" ]; then
+			WSREP_START_POSITION="$("$@" --wsrep-recover --log-error=/dev/stdout 2>&1 | grep 'Recovered position' | awk '{print $NF}')" 
+			echo WSREP: Using start position ${WSREP_START_POSITION}
+			set -- "$@" --wsrep_start_position=${WSREP_START_POSITION}
+		fi
 	fi
 	
 	chown -R mysql:mysql "$DATADIR"
 
-	set -- "$@" \
+	if [ -n "$GALERA" ]; then
+		# append galera specific run options
+
+		set -- "$@" \
 		--wsrep_cluster_name="$CLUSTER_NAME" \
 		--wsrep_cluster_address="$CLUSTER_ADDRESS" \
 		--wsrep_node_name="$NODE_NAME" \
 		--wsrep_sst_auth="replication:$REPLICATION_PASSWORD"
+	fi
 
 fi
 
